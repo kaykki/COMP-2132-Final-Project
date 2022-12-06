8e7d48ab425819b1ed7755bf3840702d51eee367
Kaki Kagatan <kkagatan1@my.bcit.ca>
2022-12-05 18:05:28 -0800

    Made a Dice Game
    implemented rolling the dice, calculating points.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,115 @@
-const $keyboard = $(`#keyboard`);
+const $playerDices        = $(`#playerDices`);
+const $playerDiceImgs     = $(`#playerDices img`);
+const $playerCurrentScore = $(`#playerCurrentScore`);
+const $playerFinalScore   = $(`#playerFinalScore`);
+
+const $cpuDices        = $(`#cpuDices`);
+const $cpuDiceImgs     = $(`#cpuDices img`);
+const $cpuCurrentScore = $(`#cpuCurrentScore`);
+const $cpuFinalScore   = $(`#cpuFinalScore`);
+
+const defaultDices = `images/dices/rolling-dices.png`;
+
+const $roll    = $(`#roll`);
+const $newGame = $(`#newGame`);
+
+let   currentRoll = 1;
+const finalRoll   = 3;
+
+let playerFinalScore = 0;
+let cpuFinalScore    = 0;
 
 
-const lettersUrl = `data/letters.json`;
+$roll.click(function(){
+    if(currentRoll <= finalRoll){
+
+        const player = rollPairOfDice();
+        const cpu    = rollPairOfDice();
+        
+        $playerDiceImgs.each(function(img){
+
+            $(this).attr(`src`, displayDice(player[img]));
+        })
+
+        $cpuDiceImgs.each(function(img){
+
+            $(this).attr(`src`, displayDice(cpu[img]));
+        })
+
+        playerFinalScore += calculateScore(player);
+        cpuFinalScore    += calculateScore(cpu);
+
+        $playerCurrentScore.text(`${calculateScore(player)}`);
+        $playerFinalScore.text(`${playerFinalScore}`);
+
+        $cpuCurrentScore.text(`${calculateScore(cpu)}`);
+        $cpuFinalScore.text(`${cpuFinalScore}`);
 
 
-fetch(lettersUrl)
-    .then(function(response){
-        if(response.ok){
-            return response.json();
-        }else{
-            console.log("Network error: fetch failed");
-        }
-    })
-    .then(function(data){
 
-        const letters = data.map(x => x);
+        currentRoll++;
+    }
+});
 
-        letters.forEach(function(letter){
-            $keyboard.append(`
-            <input type="button" value="${letter}"></input>`);
-        });
-
-    }).catch(function(error){
-        console.log(error);
+$newGame.click(function(){
+    returnDefault();
 })
+
+
+/**
+ * Rolls a pair of dices
+ * @returns an array that contains 2 random integers 1 -> 6
+ */
+function rollPairOfDice (){
+    const firstDice  = Math.floor(Math.random() * 6) + 1;
+    const secondDice = Math.floor(Math.random() * 6) + 1;
+
+    return [firstDice, secondDice];
+}
+
+/**
+ * Generates an img element for the desired dice
+ * @param {*} dice 
+ * @returns img element of the desired dice
+ */
+function displayDice ( dice = 0) {
+    return `images/dices/dice_${dice}.png`
+}
+
+/**
+ * Calculates the score of the given diceroll
+ * @param {*} diceRolls 
+ * @returns the score of the diceroll
+ */
+function calculateScore ( diceRolls = []){
+    if(diceRolls[0] == 1 || diceRolls[1] == 1) {
+        return 0;
+    } else if(diceRolls[0] == diceRolls[1]) {
+        return (diceRolls[0] + diceRolls[1]) * 2;
+    } else {
+        return diceRolls[0] + diceRolls[1];
+    }
+}
+
+
+/**
+ * Return all the games parameters to default
+ */
+function returnDefault(){
+    currentRoll = 1;
+    $playerDiceImgs.each(function(img){
+
+        $(this).attr(`src`, defaultDices);
+    })
+
+    $cpuDiceImgs.each(function(img){
+
+        $(this).attr(`src`, defaultDices);
+    })
+
+    $(`span`).text(0);
+
+
+}
+
 
